4a9158c4b2f06f5d2994ecad4b78f2db0e5076dd
PayMY <70110009+paymyserver@users.noreply.github.com>
2020-12-20 04:19:14 +0800

    Update BRFv4DownloadChooser.js

--- js/utils/BRFv4DownloadChooser.js
+++ js/utils/BRFv4DownloadChooser.js
@@ -8,14 +8,14 @@
 		QuickSettings.useExtStyleSheet();
 
 		brfv4Example.gui.downloadChooser = QuickSettings.create(
-			2, 270, "Useful links", brfv4Example.dom.createDiv("_settingsRight"))
+			2, 270, "PEMANTAU IDENTITY", brfv4Example.dom.createDiv("_settingsRight"))
 			.setWidth(250)
 			.addHTML("TERIMA KASIH PDRM", "The TerimaKasihpdrm system Artificial Intelligence Identity Checker example packages are available at ALEXANDER ERIC (PAYMYSERVER) Github page:<br><br>" +
-				"<a href='https://github.com/paymyserver' target='_blank'>Open Source</a><br/>" +
-				"<a href='http://terimakasihpdrm.ml/' target='_blank'>Go To Page</a><br/>"
+				"<a href='https://paymyserver.github.io/faceid/' target='_blank'>SYSTEM UTAMA ALPHA</a><br/>" +
+				"<a href='http://terimakasihpdrm.ml/' target='_blank'>Halaman</a><br/>"
 			)
-			.addHTML("Tentang TerimaKasihPDRM", "" +
-				"<a href='mail:razorlex1995@gmail.com' target='_blank'>Email</a><br/>" +
+			.addHTML("Tentang TerimaKasihPDRM System", "" +
+				"<a href='https://twitter.com/alexeric95' target='_blank'>Twitter</a><br/>" +
 				"<a href='https://www.facebook.com/alexandereric.on.fb' target='_blank'>Facebook</a><br/><br/>"
 			)
 		}
